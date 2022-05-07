9ca0a1c9143171483ff604c5f8021a6126b6b053
VictorHCH <82013957+VictorHCH@users.noreply.github.com>
2022-05-06 21:12:35 -0600

    versión 5.0
    Agregar usuarios

--- agregarUsuarios.php
+++ agregarUsuarios.php
@@ -12,8 +12,25 @@
     switch ($accion){
         case 1:
             $usuario = filter_input(0, "usuario");
-            $t = filter_input(0, "tipo");
-            if($tipo)
+            $tipo = filter_input(0, "tipo");
+            $pass1 = filter_input(0, "pass1");
+            $pass2 = filter_input(0, "pass2");
+            if($usuario != null && $tipo != null && $pass1 != null && $pass2 != null){
+                $usuario = substr(trim($usuario).'            ',0,12);
+                if($tipo == 0){
+                    $tipo = 'u';
+                }
+                else{
+                    $tipo = 'a';   
+                }
+                $tipo = substr(trim($tipo).' ',0,1);
+                $pass1 = substr(trim($pass1).'   ',0,3);
+                $dato = $usuario.$tipo.$pass1;
+                $archivo = fopen("usuarios.dat", "a");
+                fwrite($archivo, $dato);
+                fclose($archivo);
+            }
+            header("Location: usuarios.php");
             break;
         case 2:
             header("Location: usuarios.php");
